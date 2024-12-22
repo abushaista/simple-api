8c48d952754c8967d76cba826013df0318cbf768
arif.hidayat <arif.sarbini@gmail.com>
2024-12-22 08:10:39 +0700

    modify model

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,28 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+            $table->id(); // INT Primary Key, Auto Increment
+            $table->string('email', 255)->unique()->nullable(false); // VARCHAR(255) Unique, Not Null
+            $table->string('password', 255)->nullable(false); // VARCHAR(255) Not Null
+            $table->string('name', 255)->nullable(false); // VARCHAR(255) Not Null
+            $table->boolean('active')->default(true); // BOOLEAN Default: true
+            $table->timestamp('created_at')->useCurrent(); // DATETIME Default: Current Timestamp
+            $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
         });
     }
 
@@ -43,7 +28,5 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
     }
 };
